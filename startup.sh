02ee25f1193a5aa5b95bd92734793657bdb1dd8d
Cody Sehl <cody.sehl@gmail.com>
2016-08-31 08:01:04 -0600

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -34,6 +34,12 @@ sudo apt-get -y install \
 git clone https://github.com/direnv/direnv ~/bin/direnv
 make -C ~/bin/direnv install
 
+# remove default apps
+sudo apt-get -y autoremove \
+    firefox gnome-calendar xterm \
+    gnome-mahjongg gnome-mines gnome-sudoku \
+    thunderbird libreoffice
+
 ## configure terminal
 # set fish as default shell
 chsh -s `which fish`
@@ -48,17 +54,40 @@ gconftool-2 --set /apps/gnome-terminal/profiles/Default/font --type string "Mona
 gconftool-2 --set /apps/gnome-terminal/profiles/Default/use_system_font --type=boolean false
 
 # install Node
+# https://github.com/creationix/nvm
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.6/install.sh | bash
 nvm install node
 
 # install Ruby
+# https://github.com/postmodern/chruby
 wget -O /tmp/chruby-0.3.9.tar.gz https://github.com/postmodern/chruby/archive/v0.3.9.tar.gz
 tar -xzvf /tmp/chruby-0.3.9.tar.gz
 sudo make -C /tmp/chruby-0.3.9 install
-
+# https://github.com/JeanMertz/chruby-fish
 wget -O /tmp/chruby-fish-0.8.0.tar.gz https://github.com/JeanMertz/chruby-fish/archive/v0.8.0.tar.gz
 tar -xzvf /tmp/chruby-fish-0.8.0.tar.gz
 sudo make -C /tmp/chruby-fish-0.8.0 install
+
+# add unity launcher shortcuts
+gsettings set com.canonical.Unity.Launcher favorites "[\
+'application://ubiquity.desktop', \
+'application://google-chrome.desktop'\
+'application://org.gnome.Nautilus.desktop', \
+'application://sublime_text.desktop'\
+'application://gnome-terminal.desktop'
+'unity://running-apps']"
+
+# configure unity launcher by uninstalling desktop search scopes (e.g., searching amazon from the unity launcher)
+# http://askubuntu.com/questions/362549/how-to-disable-all-scopes-filters-and-dash-plugins
+sudo apt-get remove $(dpkg --get-selections | cut -f1 | grep -P "^unity-(lens|scope)-" | grep -vP "unity-(lens|scope)-(home|applications|files)" | tr "\n" " ");
+# searching in the launcher only searches installed applications
+gsettings set com.canonical.Unity.Lenses always-search "['applications.scope']";
+gsettings set com.canonical.Unity.Dash scopes "['home.scope', 'applications.scope', 'files.scope']";
+
+# configure global keyboard shortcuts
+
+# screen dimming
+gsettings set org.gnome.desktop.session idle-delay 3600 # screen will dim and lock after 1 hr
 
 # config
 ln -s ~/Dropbox/ubuntu-config/.config ~/.config
@@ -69,7 +98,7 @@ ln -s ~/Dropbox/ubuntu-config/.fonts ~/.fonts
 # requires clicks
 sudo apt-get install -y ubuntu-restricted-extras
 
-# prompt to install android studio and sdk
+# install android studio and sdk
 echo ""
 echo ""
 echo ""
@@ -79,9 +108,7 @@ echo ""
 echo "Download nvidia drivers"
 echo "http://www.nvidia.com/download/driverResults.aspx/77525/en-us"
 
-# prompt for a reboot
+# reboot
 echo ""
-echo "===================="
-echo " TIME FOR A REBOOT! "
-echo "===================="
+echo "Reboot machine"
 echo ""
